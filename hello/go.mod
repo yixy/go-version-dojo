b745038c252c781e9a1ba53162f6b8ebcaa04799
yixy <youzhilane01@gmail.com>
2021-07-10 23:05:49 +0800

    hello/v1.0.1
    Signed-off-by: yixy <youzhilane01@gmail.com>

--- hello/go.mod
+++ hello/go.mod
@@ -3,3 +3,5 @@ module github.com/yixy/go-version-dojo/hello
 go 1.16
 
 require github.com/yixy/go-version-dojo v1.0.1
+
+replace github.com/yixy/go-version-dojo v1.0.1 => github.com/yixy/go-version-dojo v1.0.2
--- hello/hello.go
+++ hello/hello.go
@@ -2,5 +2,5 @@ package hello
 import "fmt"
 import "github.com/yixy/go-version-dojo/version"
 func SayHello(){
-	fmt.Println("hello-v1.0.0",version.EchoVersion())
+	fmt.Println("hello-v1.0.1",version.EchoVersion())
 }
